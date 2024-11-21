d08b7907461b86fe432b5580ac28e9c94a60a2d2
boybeak <boybeak@gmail.com>
2024-11-21 18:51:18 +0800

    move back to jekyll

--- themes/hexober/theme.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Theme configuration
-    title: 'My Custom Theme',
-    subtitle: 'A beautiful Hexo theme',
-    logo: '/images/logo.png',
-    author: 'Your Name'
-    // ... other configurations
-  };
-  
