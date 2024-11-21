398b00f3efe330bee216c467eb25c1baff47ea4f
boybeak <boybeak@users.noreply.github.com>
2024-11-21 11:02:40 +0000

    deploy: c3e95b8c2788a2c17344ed553b7441661e28296e

---
+++ assets/js/sobekyll.js
@@ -0,0 +1,12 @@
+onPageLoad();
+function onPageLoad() {
+    // document.addEventListener("DOMContentLoaded", (event) => {
+    //     document.querySelectorAll("pre code").forEach((block) => {
+    //         hljs.highlightElement(block);
+    //     });
+    // });
+}
+
+function selfOpen(url) {
+    window.open(url, '_self');
+}
